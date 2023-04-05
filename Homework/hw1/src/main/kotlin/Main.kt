d64aae4201629018c5a44a8835b09c227a439953
Константин <serebryakov.kostya@gmail.com>
2023-04-05 18:05:00 +0300

    Дз1 (исправлено)

--- Homework/hw1/src/main/kotlin/Main.kt
+++ Homework/hw1/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
-class Arr(private val array: Array<Int>) {
+class ArrayUtils(private val array: Array<Int>) {
     fun sort() {
-        var needSort: Boolean = true
+        var needSort = true
 
         while (needSort) {
             needSort = false
@@ -8,41 +8,42 @@ class Arr(private val array: Array<Int>) {
             for (i in 1 until array.size) {
                 if (array[i - 1] > array[i]) {
                     needSort = true
-                    val prevEl = array[i - 1]
+                    val previousElement = array[i - 1]
                     array[i - 1] = array[i]
-                    array[i] = prevEl
+                    array[i] = previousElement
                 }
             }
         }
     }
 
     fun min(): Int {
-        var minim: Int = array[0]
+        var minmum = array[0]
 
         for (el in array)
-            if (minim < el)
-                minim = el
-        return minim
+            if (minmum < el)
+                minmum = el
+        return minmum
     }
 
     fun max(): Int {
-        var maxim: Int = array[0]
+        var maximum = array[0]
 
         for (el in array)
-            if (maxim > el)
-                maxim = el
-        return maxim
+            if (maximum > el)
+                maximum = el
+        return maximum
     }
 
     fun print() {
-        for (el in array)
+        for (el in array) {
             print("$el \t")
+        }
     }
 
 }
 
 fun main(args: Array<String>) {
-    val newArr : Arr = Arr(arrayOf(5, 6, 1, 3, 6, 0))
+    val newArr = ArrayUtils(arrayOf(5, 6, 1, 3, 6, 0))
     println(newArr.max())
     println(newArr.min())
     newArr.sort()
