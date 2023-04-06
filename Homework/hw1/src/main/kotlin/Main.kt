060aa590e72a951e6ef8aa182e42abf94e2dd6b4
bsktblman <bsktblman@yandex.ru>
2023-04-06 18:35:28 +0300

    дз 1

--- Homework/hw1/src/main/kotlin/Main.kt
+++ Homework/hw1/src/main/kotlin/Main.kt
@@ -1,3 +1,38 @@
 fun main(args: Array<String>) {
-    println("Hello World kt!")
+    print("Введите целочисленный массив через пробел: ")
+    val arr = readln().split(' ').map { it.toInt() }.toIntArray()
+    val t = ArrayTools(arr)
+    println("Исходный массив: " + arr.contentToString())
+    println("Минимум: " + t.findMin())
+    println("Максимум: " + t.findMax())
+
+    val sortedArr = t.sortArr()
+    println("Отсортированный массив: " + sortedArr.contentToString())
+}
+
+class ArrayTools(val x: IntArray = intArrayOf()){
+    fun findMin(): Int {
+        var res = x[0];
+        for (i in x.drop(1))
+            if (i < res)
+                res = i
+        return res
+    }
+
+    fun findMax(): Int {
+        var res = x[0];
+        for (i in x.drop(1))
+            if (i > res)
+                res = i
+        return res
+    }
+
+    fun sortArr(): IntArray {
+        val tmp = x.clone()
+        for (i in 0 until tmp.size - 1)
+            for (j in 0 until tmp.size - i - 1)
+                if (tmp[j] > tmp[j + 1])
+                    tmp[j] = tmp[j + 1].also { tmp[j + 1] = tmp[j] }
+        return tmp
+    }
 }
