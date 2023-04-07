7f016181bdb28d5a4eb8ab90a999a71a24f37c84
bsktblman <bsktblman@yandex.ru>
2023-04-07 10:05:30 +0300

    дз 1

--- Homework/hw1/src/main/kotlin/Main.kt
+++ Homework/hw1/src/main/kotlin/Main.kt
@@ -1,38 +1,46 @@
 fun main(args: Array<String>) {
     print("Введите целочисленный массив через пробел: ")
     val arr = readln().split(' ').map { it.toInt() }.toIntArray()
-    val t = ArrayTools(arr)
+    val t = ArrayTools(arr.clone())
     println("Исходный массив: " + arr.contentToString())
     println("Минимум: " + t.findMin())
     println("Максимум: " + t.findMax())
 
-    val sortedArr = t.sortArr()
-    println("Отсортированный массив: " + sortedArr.contentToString())
+    t.sort()
+    println("Отсортированный массив: " + t.getValue().contentToString())
 }
 
-class ArrayTools(val x: IntArray = intArrayOf()){
+class ArrayTools(private val x: IntArray = intArrayOf()){
     fun findMin(): Int {
         var res = x[0];
-        for (i in x.drop(1))
-            if (i < res)
+        for (i in x.drop(1)) {
+            if (i < res) {
                 res = i
+            }
+        }
         return res
     }
 
+    fun getValue(): IntArray {
+        return x
+    }
     fun findMax(): Int {
         var res = x[0];
-        for (i in x.drop(1))
-            if (i > res)
+        for (i in x.drop(1)) {
+            if (i > res) {
                 res = i
+            }
+        }
         return res
     }
 
-    fun sortArr(): IntArray {
-        val tmp = x.clone()
-        for (i in 0 until tmp.size - 1)
-            for (j in 0 until tmp.size - i - 1)
-                if (tmp[j] > tmp[j + 1])
-                    tmp[j] = tmp[j + 1].also { tmp[j + 1] = tmp[j] }
-        return tmp
+    fun sort(){
+        for (i in 0 until x.size - 1) {
+            for (j in 0 until x.size - i - 1) {
+                if (x[j] > x[j + 1]) {
+                    x[j] = x[j + 1].also { x[j + 1] = x[j] }
+                }
+            }
+        }
     }
 }
