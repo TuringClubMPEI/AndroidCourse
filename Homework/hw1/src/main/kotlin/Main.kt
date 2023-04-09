1b08f2068fa46200ce8753427a90926d0486b817
lvov_s <lvss2003@mail.ru>
2023-04-09 13:11:22 +0300

    Выполнил ДЗ№1, создал класс ArrayTools с 3 функциями, реализующими поиск мин., макс. элемента, сортировку массива.

--- Homework/hw1/src/main/kotlin/Main.kt
+++ Homework/hw1/src/main/kotlin/Main.kt
@@ -1,49 +1,63 @@
 fun main() {
     println("Домашнее задание №1:")
-    val mas: Array<Int> = arrayOf(9, 5 , -10, 0, -2, 7)
-    val numbers: Array<Int> = arrayOf(29, 9, -10, 0, -200, 37, 7, -7, -11, -6)
-    val sample = ArrayTools()
-    sample.vivod(mas)
-    sample.min(mas)
-    sample.max(mas)
-    sample.sortmax(numbers)
-    print("\nМассив после сортировки:")
-    sample.vivod(numbers)
+    val numbers: Array<Int> = arrayOf(29, 29, -10, 0, -200, 37, 7, -7, -11, -6)
+    val sample = ArrayTools(numbers)
+    print("Исходный массив: ")
+    sample.vivod()
+    sample.min()
+    sample.get_min()
+    sample.max()
+    sample.get_max()
+    sample.sort()
+    print("Массив после сортировки:")
+    sample.vivod()
 }
-class ArrayTools{
-    fun vivod(mass: Array<Int>){
-        print("Исходный массив: ")
-        for (i in mass){
-            print(" $mass ")
+
+class ArrayTools(num: Array<Int>) {
+    private val mas: Array<Int> = num
+    private var test_znach: Int =
+        mas[0] //инициализируем переменную, которая служит мин./макс. значением в соответсвующих функциях
+
+    fun vivod() {
+        for (i in mas) {
+            print(" $i ")
         }
+        println()
     }
-    fun min(mass: Array<Int>){ //поиск минимального элемента
-        var minimum = mass[0]
-        for (i in 1..mass.size-1){
-            if (mass[i] <= minimum){
-                minimum = mass[i]
+
+    fun min() { //поиск минимального элемента
+        for (i in 1..mas.size - 1) {
+            if (mas[i] <= test_znach) {
+                test_znach = mas[i]
             }
         }
     }
 
-    fun max(mass: Array<Int>){ // поиск максимального элемента
-        var maxim = mass[0]
-        for (i in 1..mass.size-1){
-            if (mass[i] >= maxim){
-                maxim = mass[i]
+    fun get_min() {
+        println("Минимальное значение массива: $test_znach")
+    }
+
+    fun max() { // поиск максимального элемента
+        for (i in 1..mas.size - 1) {
+            if (mas[i] >= test_znach) {
+                test_znach = mas[i]
             }
         }
     }
 
-    fun sortmax(mass: Array<Int>) { //функция сортировки пузырьком
+    fun get_max() {
+        println("Максимальное значение массива: $test_znach")
+    }
+
+    fun sort() { //функция сортировки пузырьком
 
         var buff: Int
         for (i in 0..8) {
             for (j in 0..8 - i) {
-                if (mass[j] > mass[j + 1]) {
-                    buff = mass[j]
-                    mass[j] = mass[j + 1]
-                    mass[j + 1] = buff
+                if (mas[j] > mas[j + 1]) {
+                    buff = mas[j]
+                    mas[j] = mas[j + 1]
+                    mas[j + 1] = buff
                 }
             }
         }
