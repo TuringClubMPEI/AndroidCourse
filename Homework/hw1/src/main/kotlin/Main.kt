5b1a2e0c451b4e596ee7700e907088c940b5a4be
Alex <A.kotovs123@gmail.com>
2023-04-09 18:47:25 +0300

    Hw1 Kotlin

--- Homework/hw1/src/main/kotlin/Main.kt
+++ Homework/hw1/src/main/kotlin/Main.kt
@@ -1,3 +1,42 @@
-fun main(args: Array<String>) {
-    println("Hello World kt!")
+class Problem(){
+    val numbers = arrayOf(3, 2, 3, 4, -76, 123, 45, 23, -100)
+
+    fun min(){
+        var minpos =0
+        for(i in numbers){
+            if (i<i+1){
+                minpos = i
+            }
+        }
+
+        for(i in numbers){
+            if (i<minpos){
+                minpos = i
+            }
+        }
+        println("Минимальное число = $minpos")
+    }
+
+    fun max(){
+        var maxpos =0
+        for(i in numbers){
+            if (i>i+1){
+                maxpos = i
+            }
+        }
+
+        for(i in numbers){
+            if (i>maxpos){
+                maxpos = i
+            }
+        }
+        println("Максимальное число = $maxpos")
+    }
+}
+
+fun main() {
+    val pusk = Problem()
+
+    pusk.min()
+    pusk.max()
 }
