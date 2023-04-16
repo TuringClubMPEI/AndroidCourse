ed879ff842d9f767d1eb62327db14cf18d1cfe8c
acediat <128941974+acediat@users.noreply.github.com>
2023-04-16 03:33:06 +0300

    Урок 3 базовый код

---
+++ Homework/hw3/src/main/kotlin/SimpleCalculator.kt
@@ -0,0 +1,11 @@
+class SimpleCalculator: Executor {
+
+    override fun setOperation(operation: (a: Double, b: Double) -> Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun execute(a: Double, b: Double): Double {
+        TODO("Not yet implemented")
+    }
+
+}
